76ebeb6e0b90d9b95ccad0b0ccd48d6ec8bee338
ArthurGraciani <arthur.graciani@gmail.com>
2026-03-23 21:35:59 -0300

    feat: product entity

--- src/main/java/br/com/fiap/marketPlaceAGL/models/Product.java
+++ src/main/java/br/com/fiap/marketPlaceAGL/models/Product.java
@@ -1,4 +1,28 @@
 package br.com.fiap.marketPlaceAGL.models;
 
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "TB_AGL_PRODUTO")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_produto", insertable = false)
+    private Long idProduto;
+
+    @Column(name = "nome_produto")
+    private String nomeProduto;
+
+    @Column(name = "preco_produto")
+    private float precoProduto;
+
+    @Column(name = "quantidade_estoque")
+    private int quantidadeEstoque;
+
+    @Column(name = "produto_disponivel")
+    private char produtoDisponivel;
+
 }
