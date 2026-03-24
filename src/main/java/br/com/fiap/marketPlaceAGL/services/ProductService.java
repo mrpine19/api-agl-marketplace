60985b8b547a30bf57c614e0fe645e76e42ee2eb
ArthurGraciani <arthur.graciani@gmail.com>
2026-03-23 21:37:16 -0300

    feat: product service

--- src/main/java/br/com/fiap/marketPlaceAGL/services/ProductService.java
+++ src/main/java/br/com/fiap/marketPlaceAGL/services/ProductService.java
@@ -1,4 +1,47 @@
 package br.com.fiap.marketPlaceAGL.services;
 
+import br.com.fiap.marketPlaceAGL.models.Product;
+import br.com.fiap.marketPlaceAGL.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository repository;
+
+    public List<Product> getProduct(){
+        return repository.findAll();
+    }
+
+    public Product addProduct(Product product){
+        return repository.save(product);
+    }
+
+    public Optional<Product> getProductById(Long id){
+        return repository.findById(id);
+    }
+
+    public void deleteProduct(Long id) {
+        var optionalProduct = getProductById(id);
+        if (optionalProduct.isEmpty()){
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found");
+        }
+        repository.deleteById(id);
+    }
+
+    public Product updateProduct(Long id, Product newProduct) {
+        var optionalProduct = getProductById(id);
+        if (optionalProduct.isEmpty()) {
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Product e not found");
+        }
+        newProduct.setIdProduto(id);
+        return repository.save(newProduct);
+    }
 }
