c955d35212cd9b68411ce277e597d04282dc7ccf
ArthurGraciani <arthur.graciani@gmail.com>
2026-03-23 21:36:58 -0300

    feat: product repository

---
+++ src/main/java/br/com/fiap/marketPlaceAGL/repository/ProductRepository.java
@@ -0,0 +1,7 @@
+package br.com.fiap.marketPlaceAGL.repository;
+
+import br.com.fiap.marketPlaceAGL.models.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Long> {
+}
--- src/main/java/br/com/fiap/marketPlaceAGL/repository/ProjectRepository.java
+++
@@ -1,4 +0,0 @@
-package br.com.fiap.marketPlaceAGL.repository;
-
-public class ProjectRepository {
-}
