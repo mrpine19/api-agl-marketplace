ff0c73d89630f7d4970fe71937291eb492d1eff0
ArthurGraciani <arthur.graciani@gmail.com>
2026-03-23 21:37:27 -0300

    feat: product controller

--- src/main/java/br/com/fiap/marketPlaceAGL/controllers/ProductController.java
+++ src/main/java/br/com/fiap/marketPlaceAGL/controllers/ProductController.java
@@ -1,4 +1,51 @@
 package br.com.fiap.marketPlaceAGL.controllers;
 
+import br.com.fiap.marketPlaceAGL.models.Product;
+import br.com.fiap.marketPlaceAGL.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @GetMapping
+    public List<Product> getProduct(){
+        return service.getProduct();
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id){
+        return service.getProductById(id)
+                .map((m) -> ResponseEntity.ok(m))
+                .orElse(ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Product> addProduct(@RequestBody Product product){
+        var newProduct = service.addProduct(product);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(newProduct);
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
+        Product product = service.updateProduct(id, newProduct);
+        return ResponseEntity.ok(product);
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        service.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+
 }
