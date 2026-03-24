fa56709bc5f112b2af38b5d9c5d8b9c545e6bca6
ArthurGraciani <arthur.graciani@gmail.com>
2026-03-23 22:35:46 -0300

    feat: product logger

--- src/main/java/br/com/fiap/marketPlaceAGL/controllers/ProductController.java
+++ src/main/java/br/com/fiap/marketPlaceAGL/controllers/ProductController.java
@@ -2,6 +2,8 @@ package br.com.fiap.marketPlaceAGL.controllers;
 
 import br.com.fiap.marketPlaceAGL.models.Product;
 import br.com.fiap.marketPlaceAGL.services.ProductService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +15,20 @@ import java.util.List;
 @RequestMapping("/products")
 public class ProductController {
 
+    private final Logger log = LoggerFactory.getLogger(getClass());
+
     @Autowired
     private ProductService service;
 
     @GetMapping
     public List<Product> getProduct(){
+        log.info("Listing all products");
         return service.getProduct();
     }
 
     @GetMapping("{id}")
     public ResponseEntity<Product> getProductById(@PathVariable Long id){
+        log.info("Listing product with id: " + id);
         return service.getProductById(id)
                 .map((m) -> ResponseEntity.ok(m))
                 .orElse(ResponseEntity.notFound().build());
@@ -30,6 +36,7 @@ public class ProductController {
 
     @PostMapping
     public ResponseEntity<Product> addProduct(@RequestBody Product product){
+        log.info("Registering product: " + product);
         var newProduct = service.addProduct(product);
         return ResponseEntity
                 .status(HttpStatus.CREATED)
@@ -38,12 +45,14 @@ public class ProductController {
 
     @PutMapping("{id}")
     public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product newProduct){
+        log.info("Updating product with id: " + id);
         Product product = service.updateProduct(id, newProduct);
         return ResponseEntity.ok(product);
     }
 
     @DeleteMapping("{id}")
     public ResponseEntity<Void> deleteProduct(@PathVariable Long id){
+        log.info("Deleting product with id: " + id);
         service.deleteProduct(id);
         return ResponseEntity.noContent().build();
     }
