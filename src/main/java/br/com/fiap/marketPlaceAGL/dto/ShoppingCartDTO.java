000e964f446f30889b5d8f75f2bc842697c75c97
Gustavo Pinheiro <mrpine19@gmail.com>
2026-03-29 15:20:12 -0300

    tests: criando testes unitários para ShoppingCartController

--- src/main/java/br/com/fiap/marketPlaceAGL/dto/ShoppingCartDTO.java
+++ src/main/java/br/com/fiap/marketPlaceAGL/dto/ShoppingCartDTO.java
@@ -1,8 +1,10 @@
 package br.com.fiap.marketPlaceAGL.dto;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class ShoppingCartDTO {
     private long idCustomer;
     private long idProduct;
